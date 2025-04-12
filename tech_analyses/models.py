404ed00ac020ac378ee492700355c75aec1ef144
Vokerlee <vokerlee@gmail.com>
2025-04-12 21:25:23 +0300

    Apps: add technical indicator as a Django model

---
+++ tech_analyses/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class TechnicalIndicator(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    formula = models.TextField(verbose_name="Формула")
+    recommendation = models.TextField(verbose_name="Рекомендации по использованию")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Технический индикатор"
+        verbose_name_plural = "Технические индикаторы"
